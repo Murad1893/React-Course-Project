1c21b4f6c9ce0a24344468871f248a9e783625dc
Murad1893 <muradmansoor189@gmail.com>
2020-04-08 22:51:38 +0500

    Single page applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,10 +3,15 @@ import Menu from './MenuComponent' //importing my menu component
 import '../App.css';
 import Dishdetail from './DishdetailComponent'
 import { DISHES } from '../shared/dishes'
+import { COMMENTS } from '../shared/comments'
+import { LEADERS } from '../shared/leaders'
+import { PROMOTIONS } from '../shared/promotions'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import Contact from './ContactComponent'
+
 
 //This is a container component handling all the state and passing it onto presentational components in order for them to display
 class Main extends Component {
@@ -16,9 +21,11 @@ class Main extends Component {
 
     this.state = {//over here we can define variables related to this component
       //defining as a javascript object containing a list of dishes
-      dishes: DISHES, //now we have lifted all the information into the parent component
-      selectedDish: null
-    }
+      dishes: DISHES,//now we have lifted all the information into the parent component
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+    };
   }
 
   onDishSelect(dishId) {
@@ -33,7 +40,11 @@ class Main extends Component {
     //making a functional component here
     const HomePage = () => {
       return (
-        <Home></Home>
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       )
     }
 
@@ -45,6 +56,7 @@ class Main extends Component {
           {/**if we want to pass some props a component we must send it using an arrow function */}
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}></Menu>} /> {/**here route should match the exact pathname */}
           {/**when the above routes are not matched the user is redirected to a default page */}
+          <Route exact path='/contactus' component={Contact}></Route>
           <Redirect to='/home'></Redirect> {/**default redirection to home */}
         </Switch>
         <Footer></Footer>
