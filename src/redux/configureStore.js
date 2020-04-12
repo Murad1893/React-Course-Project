abf055573a044909e814e368074e5ff1e4501c7e
Murad1893 <muradmansoor189@gmail.com>
2020-04-12 14:33:03 +0500

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,21 @@
 //Done as per approach on coursera. We can export this configuration
-import { createStore } from 'redux'
-import { Reducer, initialState } from './reducer'
+import { createStore, combineReducers } from 'redux'
+//splitted into four different reducer function and the combining them back
+import { Dishes } from './dishes'
+import { Comments } from './comments'
+import { Promotions } from './promotions'
+import { Leaders } from './leaders'
 
 export const ConfigureStore = () => {
   //enables us to create store
   const store = createStore(
-    Reducer,
-    initialState
+    //combining all the four different reducers into a single reducer
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders
+    })
   )
 
   return store
